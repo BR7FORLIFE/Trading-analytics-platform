350ddc84ebc69f0ba6fbd3b60728d34f03d79187
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-08-22 10:23:18 -0500

    feat: add fullname field to DataEconomicDraft and update economic data endpoint

--- persistance/src/main/java/com/files/persistance/models/DataEconomicDraft.java
+++ persistance/src/main/java/com/files/persistance/models/DataEconomicDraft.java
@@ -1,6 +1,6 @@
 package com.files.persistance.models;
 
 //informacion estructural para cada uno de los indicadores economicos
-public record DataEconomicDraft(String name, String priceLast, String priceChange, String priceChangePercent) {
+public record DataEconomicDraft(String name, String fullname ,String priceLast, String priceChange, String priceChangePercent) {
 
 }
